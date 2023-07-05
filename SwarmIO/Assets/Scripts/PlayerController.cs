39d20995464b4a57daa9237ef347631d33fc0921
Geming Zhu <zgming20@163.com>
2023-07-05 16:20:24 -0400

    PlayerBasicMovement
    PlayerBasicMovement

--- SwarmIO/Assets/Scripts/PlayerController.cs
+++ SwarmIO/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 
 public class PlayerController : Entity
 {
-
+    public GameObject player;
+    private Animator animator;
     public PlayerController(int health, int movementSpeed) : base(health, movementSpeed)
     {
+    }
+    private void Start()
+    {
+        animator = player.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,9 +24,33 @@ public class PlayerController : Entity
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical) * movementSpeed * Time.deltaTime;
-        this.transform.position += movement;
-        this.setHealth(3);
-
+        if (moveHorizontal != 0 || moveVertical != 0)
+        {
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical) * movementSpeed * Time.deltaTime;
+            this.transform.position += movement;
+            PlayerFlip(moveHorizontal);
+            PlayerWalk();
+        } else
+        {
+            PlayerIdle();
+        }
+    }
+    private void PlayerFlip(float moveHorizontal)
+    {
+        if (moveHorizontal < 0)
+        {
+            this.transform.localScale = new Vector3(-1, 1, 1);
+        } else if (moveHorizontal > 0)
+        {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+    private void PlayerWalk()
+    {
+        animator.SetFloat("speed", 1);
+    }
+    private void PlayerIdle()
+    {
+        animator.SetFloat("speed", 0);
     }
 }
