e2da806e8916f91ced8e34d91df26364ded5b638
Norrischen12 <norrisch@seas.upenn.edu>
2023-07-17 14:37:26 -0400

    range weapon push

--- SwarmIO/Assets/Scripts/RangeWeapon.cs
+++ SwarmIO/Assets/Scripts/RangeWeapon.cs
@@ -2,21 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangeWeapon : Weapons
+public abstract class RangeWeapon : MonoBehaviour
 {
-    public RangeWeapon(int durability) : base(durability)
-    {
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float weaponAtt;
+    public float bulletSpeed;
+    public float weaponAttSpeed;
+    public abstract void Fire();
 }
