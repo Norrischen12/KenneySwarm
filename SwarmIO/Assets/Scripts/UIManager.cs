fe1ded5e9cede69743bfd4bf12e3c3382eca8937
Norrischen12 <norrisch@seas.upenn.edu>
2023-07-20 15:31:37 -0400

    UI call to change weapon neame

--- SwarmIO/Assets/Scripts/UIManager.cs
+++ SwarmIO/Assets/Scripts/UIManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public RectTransform HPPivot;
     public RectTransform MNPivot;
+    public TextMeshProUGUI WeaponName;
+    public GameObject Player;
+
     public void setHP(float hpPoint)
     {
         Vector3 newHP = HPPivot.localScale;
@@ -17,5 +21,10 @@ public class UIManager : MonoBehaviour
         Vector3 newMN = MNPivot.localScale;
         newMN.x = mn;
         MNPivot.localScale = newMN;
+    }
+
+    public void updateWeaponName()
+    {
+        WeaponName.text = Player.GetComponent<PlayerController>().GetCurrentWeaponName();
     }
 }
