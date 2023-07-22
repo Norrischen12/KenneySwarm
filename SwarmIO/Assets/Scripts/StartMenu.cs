d093e597a30a4d059f5771399bf956bbf24bb762
Geming Zhu <zgming20@163.com>
2023-07-22 17:51:41 -0400

    tutorial

--- SwarmIO/Assets/Scripts/StartMenu.cs
+++ SwarmIO/Assets/Scripts/StartMenu.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    public GameObject tutorial;
+    private bool tutorialOn;
     public void playGame()
     {
         SceneManager.LoadScene("GamePlay");
+    }
+    public void toggleTutorial()
+    {
+        if (tutorialOn)
+        {
+            tutorial.SetActive(false);
+            tutorialOn = false;
+        }
+        else if (!tutorialOn)
+        {
+            tutorial.SetActive(true);
+            tutorialOn = true;
+        }
     }
 }
