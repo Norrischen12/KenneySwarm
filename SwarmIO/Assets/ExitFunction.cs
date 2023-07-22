e029f5f2ff3efca85248639045c9c2ff5dcd294c
Geming Zhu <zgming20@163.com>
2023-07-22 17:40:22 -0400

    exit hint

--- SwarmIO/Assets/ExitFunction.cs
+++ SwarmIO/Assets/ExitFunction.cs
@@ -7,11 +7,13 @@ public class ExitFunction : MonoBehaviour
 {
     private GameObject player;
     public GameObject congrats;
+    public GameObject hints;
     private void Start()
     {
         player = GameObject.Find("Player");
         GameObject ui = GameObject.Find("Canvas");
         congrats = ui.transform.GetChild(5).gameObject;
+        hints = ui.transform.GetChild(6).gameObject;
         
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,7 +23,15 @@ public class ExitFunction : MonoBehaviour
             player.GetComponent<PlayerController>().SetGameOver(true);
             congrats.SetActive(true);
 
+        } else if (collision.gameObject.tag == "Player" && player.GetComponent<PlayerController>().GetCurrentMushroom() < 5)
+        {
+            hints.SetActive(true);
+            StartCoroutine(hintDisappear());
         }
     }
-
+    private IEnumerator hintDisappear()
+    {
+        yield return new WaitForSeconds(1);
+        hints.SetActive(false);
+    }
 }
